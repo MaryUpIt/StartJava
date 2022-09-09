64d4053a2bbc29c7f4dde83c8d3286aa25f2d2d7
email <94126244+MaryUpIt@users.noreply.github.com>
2022-09-09 11:05:36 +0300

    #1 Урок #5 Итоговое домашнее задание Тема: "Игра: Угадай число"

---
+++ Lesson1/MyFirstGame.java
@@ -0,0 +1,34 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int random = 9;
+        int gamer = 50;
+        while (gamer != random) {
+            if (gamer > random) {
+                while (gamer > random ) {
+                    System.out.printf("Число %d больше того, что загадал компьютер\n", gamer);
+                    gamer -= 10;
+                    if (gamer == 0){
+                        while(gamer != random){
+                            gamer++;
+                            System.out.printf("Число %d меньше того, что загадал компьютер\n", gamer);
+                        }
+                    }
+                }
+                while (gamer < random ) {
+                    System.out.printf("Число %d меньше того, что загадал компьютер\n", gamer);
+                    gamer++;
+                }
+            } else {
+                while (gamer < random) {
+                    System.out.printf("Число %d меньше того, что загадал компьютер\n", gamer);
+                    gamer += 10;
+                }
+                while (gamer < random) {
+                    System.out.printf("Число %d больше того, что загадал компьютер\n", gamer);
+                    gamer--;
+                }
+            }
+        } 
+        System.out.printf("Вы победили! число %d\n", gamer);
+    }
+}
