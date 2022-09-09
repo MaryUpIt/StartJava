24d48f2dec86c5aeb3e60914030db76af35b8810
email <94126244+MaryUpIt@users.noreply.github.com>
2022-09-09 16:50:10 +0300

    #2 Урок #1 Домашнее задание. Тема: "настройка класса Person.'

---
+++ Lesson2/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    String name = "Galina";
+    boolean isMale = false;
+    int age = 35;
+    double height = 1.64;
+    double weight = 45.5;
+
+    void walk() {
+        System.out.prinln("walking");
+    }
+
+    void run() {
+        System.out.prinln("running");
+    }
+
+    void sit() {
+        System.out.prinln("sitting");
+    }
+
+    void talk() {
+        System.out.prinln("talking");
+    }
+}
