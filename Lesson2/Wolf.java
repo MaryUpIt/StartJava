6ed1a08aaca673d071f8f6424a937b192219d799
email <94126244+MaryUpIt@users.noreply.github.com>
2022-09-09 17:21:57 +0300

    #2 Урок #2 Домашнее задание. Тема: "Проектирование класса Wolf'

---
+++ Lesson2/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+    String name;
+    boolean isMale;
+    int age;
+    double weight;
+    String color;
+
+    void walk() {
+        System.out.println("Волк гуляет.");
+    }
+
+    void sit() {
+        System.out.println("Волк сидит.");
+    }
+
+    void wail() {
+        System.out.println("Волк воет.");
+    }
+
+    void run() {
+        System.out.println("Волк бежит.");
+    }
+
+    void hunt() {
+        System.out.println("Волк охотится.");
+    }
+}
