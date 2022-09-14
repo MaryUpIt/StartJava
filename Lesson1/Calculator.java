e9089a5b7d5853b6ffc3ae24aa045b9e61eb1e4e
email <94126244+MaryUpIt@users.noreply.github.com>
2022-09-14 11:37:01 +0300

    #1 Урок #4 Итоговое домашнее задание Тема: "Калькулятор" -пробел нашла -вывод сдвинула -исправила финальную строку

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 2;
         char sign = '^';
-        int b = 8;
+        int b = 1;
         int result = 0;
         if (sign == '+') {
             result = a + b;
@@ -11,27 +11,27 @@ public class Calculator {
         } else if (sign == '*') {
             result = a * b;
         } else if (sign == '/') {
-            if (b != 0){
+            if (b != 0) {
                 result = a / b;
             } else {
-        System.out.println("Делить на ноль нельзя!");
+                System.out.println("Делить на ноль нельзя!");
             }
         } else if (sign == '%') {
             if (b != 0) {
                 result = a % b;
             } else {
-        System.out.println("Делить на ноль нельзя!");
+                System.out.println("Делить на ноль нельзя!");
             }
         } else if (sign == '^') {
             result = 1;
             int tmp = b;
-            while (tmp > 1) {
+            while (tmp >= 1) {
                 result *= a;
                 tmp--;
             }
         } else {
             System.out.println("Неверный знак операции!");
         }
-        System.out.println(a + "" + sign + "" + b + "=" + result);
+        System.out.println(a + " " + sign + " " + b + " = " + result);
     }
 }
