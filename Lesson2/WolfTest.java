482edbb1bd807c36a4d180db8746190f1e990e3d
email <94126244+MaryUpIt@users.noreply.github.com>
2022-09-15 19:19:33 +0300

    #2 Урок #2 Домашнее задание. Тема: "Проектирование класса Wolf Инкапсуляция

--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -1,21 +1,25 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.name = "Гоша";
-        wolf.isMale = true;
-        wolf.age = 2;
-        wolf.weight = 47.5;
-        wolf.color = "серый";
-        if (!wolf.isMale){
-            System.out.println(wolf.name + " " + wolf.color + " самка");
+        wolf.setName("Гоша");
+        wolf.setMale(true);
+        wolf.setAge(8);
+        wolf.setWeight(47.5);
+        wolf.setColor("серый");
+        if (wolf.getAge() != 0) {
+            if (!wolf.getMale()){
+                System.out.println(wolf.getName() + " " + wolf.getColor() + " самка");
+            } else {
+                System.out.println(wolf.getName() + " " + wolf.getColor() + " самец");
+            }
+            System.out.println("возраст: " + wolf.getAge() + " вес: " + wolf.getWeight());
+            wolf.walk();
+            wolf.sit();
+            wolf.wail();
+            wolf.run();
+            wolf.hunt();
         } else {
-            System.out.println(wolf.name + " " + wolf.color + " самец");
+            System.out.println("Волк уже мертв, или еще не родился");
         }
-        System.out.println("возраст: " + wolf.age + " вес: " + wolf.weight);
-        wolf.walk();
-        wolf.sit();
-        wolf.wail();
-        wolf.run();
-        wolf.hunt();
     }
 }
