dbf91eb18ab8aba01c2ef27c2ef4a9d90ab4863e
email <94126244+MaryUpIt@users.noreply.github.com>
2022-09-15 20:10:09 +0300

    #2 Урок #3 Домашнее задание. Тема: "Проектирование класса Wolf Инкапсуляция

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,27 +1,70 @@
 public class Wolf {
-    String name;
-    boolean isMale;
-    int age;
-    double weight;
-    String color;
+    private String name;
+    private boolean male;
+    private int age;
+    private double weight;
+    private String color;
 
-    void walk() {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMale(boolean male) {
+        this.male = male;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст.");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getMale() {
+        return male;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void walk() {
         System.out.println("Волк гуляет.");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Волк сидит.");
     }
 
-    void wail() {
+    public void wail() {
         System.out.println("Волк воет.");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Волк бежит.");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("Волк охотится.");
     }
 }
