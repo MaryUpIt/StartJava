0659b5f831d1c5b52af8bd1b8b9d0bb5bc15db61
email <94126244+MaryUpIt@users.noreply.github.com>
2022-09-17 11:17:00 +0300

    #2 Урок #4 Итоговое домашнее задание. Тема: "Калькулятор форматирование switch-case

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -3,49 +3,45 @@ public class Calculator {
     private int b;
     private char sign;
 
-    public Calculator (int a, int b, char sign)
-    {
+    public Calculator(int a, int b, char sign) {
         this.a = a;
-        this.b =b;
+        this.b = b;
         this.sign = sign;
     }
 
-    public int result() {
+    public int calculating() {
         int result = 0;
         switch(sign) {
-        case '+':
-            result = a + b;
-            break;
-        case '-':
-            result = a - b;
-            break;
-        case '*':
-            result = a * b;
-            break;
-        case '/':
-            if (b != 0) {
-                result = a / b;
-            } else {
-                System.out.println("Ошибка: делить на ноль нельзя!");
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b != 0) {
+                    return a / b;
+                } else {
+                    System.out.println("Ошибка: делить на ноль нельзя!");
+                    break;
+                }
+            case '%':
+                if (b != 0) {
+                    return a % b;
+                } else {
+                    System.out.println("Ошибка: делить на ноль нельзя!");
+                    break;
+                }
+            case '^':
+                result = 1;
+                while (b >= 1) {
+                    result *= a;
+                    b--;
+                }
+                return result;
+            default:
+                System.out.println("Ошибка: знак операции неверный!");
             }
-            break;
-        case '%':
-            if (b != 0) {
-                result = a % b;
-            } else {
-                System.out.println("Ошибка: делить на ноль нельзя!");
-            }
-            break;
-        case '^':
-            result = 1 ;
-            while (b >= 1) {
-                result *= a;
-                b--;
-            }
-            break;
-        default:
-            System.out.println("Ошибка: знак операции неверный!");
-        }
         return result;
     }
 }
