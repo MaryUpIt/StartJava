47d37e62a242146b0ab394768fea6df523fba4dc
email <94126244+MaryUpIt@users.noreply.github.com>
2022-09-17 19:38:15 +0300

    #2 Урок #6 Итоговое домашнее задание. Тема: "Игра Угадай число" изменения

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-        
-
         System.out.println("Добро пожаловать на игру \"Угадай число\" ");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите имя первого игрока: ");
@@ -11,14 +9,13 @@ public class GuessNumberTest {
         System.out.print("Введите имя второго игрока: ");
         Player player2 = new Player(scanner.nextLine());
         GuessNumber game = new GuessNumber(player1, player2);
-        String answer;
-        do {
-            answer = "ok";
-            game.gamePlay();
-            while (!answer.equals("yes") && !answer.equals("no")){
+        String answer = "yes";
+        while (!answer.equals("no")) {
+            game.play();
+            do {
                 System.out.print("Перезапустить игру? [yes/no]: ");
                 answer = scanner.next();
-            }
-        } while (!answer.equals("no"));
+            } while(!answer.equals("yes") && !answer.equals("no"));
+        }
     }
 }
